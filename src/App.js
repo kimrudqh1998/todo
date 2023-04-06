6fc8d66ae711e1a16504b835f4a51b84e3b2cf6f
kimrudqh1998 <kimrudqh1998@gmail.com>
2023-04-06 15:28:17 +0900

    .;,;ㅣ;ㅣ;

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import './index.css';
 
 function App() {
 
+  //State 훅
   const [todos, setTodos] = useState([])
   const [nextId, setNextId] = useState(0);
   const [compTodos, setCompTodos] = useState(0);
 
+  //배열 생성
   const handleCreate = (newTodo) => {
       newTodo.id = nextId;
       setNextId(nextId + 1);
@@ -18,14 +20,15 @@ function App() {
         setCompTodos(compTodos+1);
       }
   };
-
+ //현재 생성된 총 할 일의 갯수
   const todoCount = todos.length;
 
+  //배열 삭제를 위한 필터
   const onRemove = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
-  
+  //수정 반영
   const handleEdit = (id, newName) => {
     const updatedTodos = todos.map((todo) =>
       todo.id === id ? { ...todo, name: newName } : todo
@@ -33,8 +36,9 @@ function App() {
     setTodos(updatedTodos);
   };
 
-
+  //리턴
   return (
+    //css 파일을 따로 만들어서 import만으로도 적용할 수 있다.
     <div className="App"
         style={{backgroundColor : "#00BD9D", border: "1px", width:"450px", borderRadius:"20px", 
         justifyContent:"center",margin:'auto', textAlign: "center"}}
@@ -48,8 +52,9 @@ function App() {
         <body>
           <p><b>남은 할 일:{todoCount}</b></p>
           <div className="centerbox"
+          /*맵을 이용해서 배열 출력, 각 버튼에 맞는 함수 지정 */ 
             style={{backgroundColor:"#218380", textAlign:"left", width:"400px", height:"auto", minHeight:"450px", borderRadius:"20px", justifyContent:"center", margin:"auto"}}>
-              <div style={{width:"380px", height:"auto",justifyContent:"center", margin:"auto", fontSize:"30px"}}>             
+              <div style={{width:"380px", height:"auto",justifyContent:"center", margin:"auto", fontSize:"30px"}}>            
                   {todos.map(todo => (
                   <TodoItem key={todo.id} todo={todo} onRemove={onRemove} onEdit={handleEdit}/>
                   ))}                
